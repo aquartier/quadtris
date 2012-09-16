464403180f0915b4d9d10a227b6ef0331e973cc3
rodjeep9 <nuttawootk@gmail.com>
2012-09-17 02:17:44 +0700

    before merge to master

--- Quadtris/src/com/dekdroid/quadtris/Quadtris.java
+++ Quadtris/src/com/dekdroid/quadtris/Quadtris.java
@@ -15,10 +15,10 @@ import com.dekdroid.quadtris.SceneManager.SceneType;
  * 
  * @modify LeoNaiDaS
  * @see http://stuartmct.co.uk/2012/07/16/andengine-scenes-and-scene-management/
- *
+ * 
  */
 
-public class Quadtris extends BaseGameActivity { //Main Activity
+public class Quadtris extends BaseGameActivity { // Main Activity
 	static final int CAMERA_WIDTH = 480;
 	static final int CAMERA_HEIGHT = 800;
 	static final int BOARD_WIDTH = 17;
@@ -31,56 +31,62 @@ public class Quadtris extends BaseGameActivity { //Main Activity
 	@Override
 	public EngineOptions onCreateEngineOptions() {
 		mCamera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
-		EngineOptions engineOptions = new EngineOptions(true, ScreenOrientation.PORTRAIT_FIXED, new FillResolutionPolicy(), mCamera);
+		EngineOptions engineOptions = new EngineOptions(true,
+				ScreenOrientation.PORTRAIT_FIXED, new FillResolutionPolicy(),
+				mCamera);
 		return engineOptions;
 	}
 
 	@Override
-	public void onCreateResources(OnCreateResourcesCallback pOnCreateResourcesCallback)	throws Exception {
-		
+	public void onCreateResources(
+			OnCreateResourcesCallback pOnCreateResourcesCallback)
+			throws Exception {
+
 		relativePos = new int[17][17];
-		
-		//Generate relative array here
-		int i,j;
-		for(i=0;i<17;i++){
-			for(j=0;j<17;j++){
-					if(i<=j)
-						relativePos[i][j] = 1;
-					else 
-						relativePos[i][j] = 0;
+
+		// Generate relative array here
+		int i, j;
+		for (i = 0; i < 17; i++) {
+			for (j = 0; j < 17; j++) {
+				if (i <= j)
+					relativePos[i][j] = 1;
+				else
+					relativePos[i][j] = 0;
 			}
 		}
-		
+
 		boardTable = new BoardTable(relativePos);
-		
-		sceneManager = new SceneManager(this, mEngine, mCamera,boardTable);
+
+		sceneManager = new SceneManager(this, mEngine, mCamera, boardTable);
 		sceneManager.loadSplashSceneResources();
 		pOnCreateResourcesCallback.onCreateResourcesFinished();
 	}
 
 	@Override
-	public void onCreateScene(OnCreateSceneCallback pOnCreateSceneCallback)	throws Exception {
-		pOnCreateSceneCallback.onCreateSceneFinished(sceneManager.createSplashScene());
+	public void onCreateScene(OnCreateSceneCallback pOnCreateSceneCallback)
+			throws Exception {
+		pOnCreateSceneCallback.onCreateSceneFinished(sceneManager
+				.createSplashScene());
 	}
 
-	//Method to choose screen to display
+	// Method to choose screen to display
 	@Override
-	public void onPopulateScene(Scene pScene,OnPopulateSceneCallback pOnPopulateSceneCallback) throws Exception {
-		mEngine.registerUpdateHandler(new TimerHandler(1f, new ITimerCallback()	{
-			public void onTimePassed(final TimerHandler pTimerHandler)
-			{
-				mEngine.unregisterUpdateHandler(pTimerHandler);
-				sceneManager.loadGameSceneResources();
-				sceneManager.createGameScenes();			//
-				sceneManager.setCurrentScene(SceneType.MAINGAME);
-			}
-		}));
-		pOnPopulateSceneCallback.onPopulateSceneFinished();	
-		
-		//Jeep code here. Do everything you want such as create thread, game logic or update blockObj.
-		
+	public void onPopulateScene(Scene pScene,
+			OnPopulateSceneCallback pOnPopulateSceneCallback) throws Exception {
+		mEngine.registerUpdateHandler(new TimerHandler(1f,
+				new ITimerCallback() {
+					public void onTimePassed(final TimerHandler pTimerHandler) {
+						mEngine.unregisterUpdateHandler(pTimerHandler);
+						sceneManager.loadGameSceneResources();
+						sceneManager.createGameScenes(); //
+						sceneManager.setCurrentScene(SceneType.MAINGAME);
+					}
+				}));
+		pOnPopulateSceneCallback.onPopulateSceneFinished();
+
+		// Jeep code here. Do everything you want such as create thread, game
+		// logic or update blockObj.
+
 	}
 
 }
-
-
